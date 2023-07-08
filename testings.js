59821e60a6373c2d6c73a8c1992f31ad12a9cb0e
Xiya Li <xiyali@Xiyas-iMac.local>
2023-07-08 20:11:39 +0800

    Initial commit

--- testings.js
+++ testings.js
@@ -1,8 +1,8 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import { DRACOLoader } from '../node_modules/three/examples/jsm/loaders/DRACOLoader.js';
-import {SkeletonUtils} from '../node_modules/three/examples/jsm/utils/SkeletonUtils.js';
+import * as THREE from '../../node_modules/three/build/three.module.js';
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from '../../node_modules/three/examples/jsm/loaders/DRACOLoader.js';
+import {SkeletonUtils} from '../../node_modules/three/examples/jsm/utils/SkeletonUtils.js';
 
 
 const clock = new THREE.Clock();
