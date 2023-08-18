5b90bd438759da20e8097d7f4be64bc4fc495b95
xiyalixiya <138500531+xiyalixiya@users.noreply.github.com>
2023-08-19 00:38:56 +0800

    8.19

--- installation.js
+++ installation.js
@@ -1,7 +1,7 @@
-import * as THREE from '../node_modules/three/build/three.module.js';
-import { OrbitControls } from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
-import {SkeletonUtils} from '../node_modules/three/examples/jsm/utils/SkeletonUtils.js';
+import * as THREE from '../../node_modules/three/build/three.module.js';
+import { OrbitControls } from '../../node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import {SkeletonUtils} from '../../node_modules/three/examples/jsm/utils/SkeletonUtils.js';
 
 const clock = new THREE.Clock();
 
